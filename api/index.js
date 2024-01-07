22fccdc5865a9f177cb598395a7d4bc9e25cc3b5
“Joelmemer” <joel1912jd@gmail.com>
2024-01-07 12:48:37 +0530

    hello there

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI)
@@ -13,3 +15,6 @@ app.listen(3000, ()=>{
     console.log('server is running on port 3000!!!');
     }
 );
+
+app.use('/api/user', userRouter);  
+app.use('/api/auth',authRouter); 
